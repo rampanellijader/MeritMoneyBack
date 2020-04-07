291a7220424bdc59752232d54e013649766c0cc0
KevinMdCardoso <gx2sistemas@gx2.com.br>
2020-04-07 19:32:26 -0300

    Merge

--- MeritMoney-back-end/src/main/java/br/com/Meritmoney/MeritMoneyBackEndApplication.java
+++ MeritMoney-back-end/src/main/java/br/com/Meritmoney/MeritMoneyBackEndApplication.java
@@ -2,9 +2,10 @@ package br.com.Meritmoney;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 
 @SpringBootApplication
-public class MeritMoneyBackEndApplication {
+public class MeritMoneyBackEndApplication extends SpringBootServletInitializer {
 
 	public static void main(String[] args) {
 		SpringApplication.run(MeritMoneyBackEndApplication.class, args);
