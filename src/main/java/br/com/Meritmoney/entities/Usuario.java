9270962204a5659defc406cfe26c58a421d04e0a
rampanellijader <jader.rampa@gmail.com>
2020-06-03 20:29:36 -0300

    configurado banco de dados PostgreSQL

--- src/main/java/br/com/Meritmoney/entities/Usuario.java
+++ src/main/java/br/com/Meritmoney/entities/Usuario.java
@@ -1,14 +1,18 @@
 package br.com.Meritmoney.entities;
 
 import java.io.Serializable;
+import java.util.List;
 
+import javax.persistence.CascadeType;
 import javax.persistence.Column;
 import javax.persistence.Entity;
+import javax.persistence.FetchType;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
 import javax.persistence.JoinColumn;
 import javax.persistence.ManyToOne;
+import javax.persistence.OneToMany;
 
 @Entity
 public class Usuario implements  Serializable {
@@ -39,6 +43,9 @@ public class Usuario implements  Serializable {
 	private Perfil perfil;
 	
 
+	 @OneToMany(mappedBy = "usuario", targetEntity= UsuarioPremio.class, fetch = FetchType.LAZY, cascade=CascadeType.ALL)
+	 private List<UsuarioPremio> UsuarioPremioList;
+	
 
 	public Usuario() {
 
